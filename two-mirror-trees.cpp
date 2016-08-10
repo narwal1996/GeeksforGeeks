b024de433eab424ab5de4b5ee56fc8d24311a4bd
narwal1996 <akshaynarwal1996@gmail.com>
2016-08-10 16:01:45 +0530

    Create two-mirror-trees.cpp

---
+++ two-mirror-trees.cpp
@@ -0,0 +1,19 @@
+/* Node structure
+struct Node
+{
+    int data;
+    struct Node* left, *right;
+}; */
+
+/* Given two trees, should return true if they are
+   mirror of each other. */
+int areMirror(Node* a, Node* b)
+{
+    if(!a && !b)
+        return 1;
+    if(!a || !b)
+        return 0;
+    if(a->data == b->data && areMirror(a->left, b->right) && areMirror(a->right, b->left))
+        return 1;
+    return 0;
+}
