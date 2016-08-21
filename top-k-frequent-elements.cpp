3acff4b1c4d58278b704e5f564991599093034ac
narwal1996 <akshaynarwal1996@gmail.com>
2016-08-21 15:19:41 +0530

    Update top-k-frequent-elements.cpp

--- top-k-frequent-elements.cpp
+++ top-k-frequent-elements.cpp
@@ -3,6 +3,16 @@
   Given a non-empty array of integers, return the k most frequent elements.
   For example,
   Given [1,1,1,2,2,3] and k = 2, return [1,2].
+  
+  1. Use a Hash table to record all words' frequency T(n) = O(n)
+  2. Choose first k elements of hash table, and restore them in one buffer (whose space = k). T(n) = O(k)
+  3. Each time, firstly we need find the current min element of the buffer, and just compare the min element of the buffer with the
+     (n - k) elements of hash table one by one. If the element of hash table is greater than this min element of buffer, then drop the
+     current buffer's min, and add the element of the hash table. So each time we find the min one in the buffer need T(n) = O(k), 
+     and traverse the whole hash table need T(n) = O(n - k). So the whole time complexity for this process is T(n) = O((n-k) * k).
+  4. After traverse the whole hash table, the result is in this buffer.
+  5. The whole time complexity: T(n) = O(n) + O(k) + O(kn - k^2) = O(kn + n - k^2 + k). Since, k is really smaller than n in general.
+     So for this solution, the time complexity is T(n) = O(kn). That is linear time, when k is really small. Is it right?
 */
 
 class Solution
